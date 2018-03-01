0ae71e86b2dd499c0d07a32d6b3d8e5e61afdb19
asixa <asixa@foxmail.com>
2018-03-02 02:26:15 +0800

    新旧逻辑运算表达式备份

--- Orange/Orange/Parse/Standard1.0/Operation/MathTree.cs
+++ Orange/Orange/Parse/Standard1.0/Operation/MathTree.cs
@@ -19,5 +19,28 @@ namespace Orange.Parse.New.Operation
         public override string ToString()=>left + " " + Op+ " " + right;
         public static LogicNode Match() => MatchTemplate<MathTree>(Match_Mul_Div, new[] { '+', '-' });
         public static LogicNode Match_Mul_Div() => MatchTemplate<MathTree>(Unary.Match, new[] { '*', '/' });
+
+        public static LogicNode Match2()
+        {
+            var expr = Match_Mul_Div();
+            while (_look.TagValue == '+' || _look.TagValue == '-')
+            {
+                var tok = _look;
+                Move();
+                expr = new MathTree(tok, expr, Match_Mul_Div());
+            }
+            return expr;
+        }
+        public static LogicNode Match_Mul_Div2()
+        {
+            var expr = Unary.Match();
+            while (_look.TagValue == '*' || _look.TagValue == '/')
+            {
+                var tok = _look;
+                Move();
+                expr = new MathTree(tok, expr, Unary.Match());
+            }
+            return expr;
+        }
     }
 }
